f439840631f192d567d3c6e6c2c4a42573a3ad04
MD Rashed Karim <rashed@MDs-MacBook-Pro.local>
2025-12-03 03:08:35 +0600

    progress

--- Assignment2.py
+++ Assignment2.py
@@ -1,9 +1,32 @@
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
+import random
 
+class Diamond:
+      def __init__(self, x1,y1,x2,y2):
+            self.x1 = x1
+            self.y1 = y1
+            self.x2 = x2
+            self.y2 = y2
+    
+      def collition(self,other):  
+            if (self.x1 < other.x2 < self.x2) or (self.x1<other.x1<self.x2):
+                if (self.y1 < other.y2 < self.y2):
+                    return True
+                else:
+                    return False
+            else:
+                return False
+            
 def find_zone(x,y):
     zone = 0
+    if x == 0:
+        if y > 0:
+            zone = 1
+        else:
+            zone = 5
+
     if abs(x) > abs(y):
         if x>0 and y>0:
             zone = 0
@@ -25,39 +48,50 @@ def find_zone(x,y):
     
     return zone
 
-def convert_between_zone0_and_zonex(x, y, zone):
-      if zone == 1:
-            temp = x
-            x = y
-            y = temp
-      elif zone == 2:
-            temp = x
-            x = y
-            y = -temp
-      elif zone == 3:
-            x = -x
-      elif zone == 4:
-            x = -x
-            y = -y
-      elif zone == 5:
-            temp = x
-            x = -y
-            y = -temp
-      elif  zone == 6:
-            temp = x
-            x = -y
-            y = -temp
-      elif zone == 7:
-            y = -y
-
-      return [x,y]
+def convert_between_zone0_and_zonex(mode, x, y, zone):
+      if mode == True:
+        if zone == 0:
+                return [x,y]
+        elif zone == 1:
+                return [y,x]
+        elif zone == 2:
+                return [y,-x]
+        elif zone == 3:
+                return [-x,y]
+        elif zone == 4:
+                return [-x,-y]
+        elif zone == 5:
+                return [-y,-x]
+        elif  zone == 6:
+                return [-y, x]
+        elif zone == 7:
+                return [x,-y]
+      else:
+        if zone == 0:
+             return [x,y]
+        elif zone == 1:
+                return [y,x]
+        elif zone == 2:
+                return [y,-x]
+        elif zone == 3:
+                return [-x,y]
+        elif zone == 4:
+                return [-x,-y]
+        elif zone == 5:
+                return [-y,-x]
+        elif  zone == 6:
+                return [y, -x]
+        elif zone == 7:
+                return [x,-y]
+          
+      
 
 def mpl(x1,y1,x2,y2):
     dx = x2-x1
     dy = y2-y1
     zone = find_zone(dx, dy)
-    x1,y1 = convert_between_zone0_and_zonex(x1,y1,zone)
-    x2,y2 = convert_between_zone0_and_zonex(x2,y2,zone)
+    x1,y1 = convert_between_zone0_and_zonex(True,x1,y1,zone)
+    x2,y2 = convert_between_zone0_and_zonex(True,x2,y2,zone)
     dx = x2-x1
     dy = y2-y1
     d = (2*dy) - dx
@@ -65,7 +99,7 @@ def mpl(x1,y1,x2,y2):
     delE = 2*dy
 
     for x in range(x1,(x2+1)):
-      draw_points(convert_between_zone0_and_zonex(x,y1,zone))
+      draw_points(convert_between_zone0_and_zonex(False, x,y1,zone))
       if d>0:
            d += delNE
            y1 += 1
@@ -75,30 +109,67 @@ def mpl(x1,y1,x2,y2):
 
 def draw_points(lst):
     x,y=lst
-    glPointSize(5)         
+    glPointSize(2)         
     glBegin(GL_POINTS)     
     glVertex2f(x, y)        
     glEnd()
 
 def setup_projection():
-    glViewport(0, 0, 500, 500)     # Define the portion of the window to render to
+    glViewport(0, 0, 500, 600)     # Define the portion of the window to render to
     glMatrixMode(GL_PROJECTION)    # Switch to the projection matrix
     glLoadIdentity()               # Reset the projection matrix
-    glOrtho(0.0, 500, 0.0, 500, 0.0, 1.0)  # Define a 2D orthographic projection
+    glOrtho(-250.0, 250, -300.0, 300, 0.0, 1.0)  # Define a 2D orthographic projection
     glMatrixMode(GL_MODELVIEW)
 
 
 def display():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)  # Clear screen and depth buffer
     glLoadIdentity()                                    # Reset transformations
-    setup_projection()                                  # Set up coordinate system
-    glColor3f(1.0, 1.0, 0.0)                            # Set color (R, G, B) → Yellow
-    mpl(100,400,300,300)                                   # Draw the point
+    setup_projection()
+    iterate()
+    draw_return()
+    draw_pause()                                 # Set up coordinate system
+    draw_cross()
+    draw_diamond()                                  # Draw the point
     glutSwapBuffers()  
+def draw_cross():
+    glColor3f(1.0, 0.0, 0.0)
+    mpl(220,270,240,290)
+    mpl(240,270,220,289)
+
+def draw_pause():
+    glColor3f(1.0,1.0,0.0)
+    mpl(-5,270,-5,290)
+    mpl(5,270,5,290)
+
+def draw_return():
+    glColor3f(0.0,0.0,1.0)
+    mpl(-240,280,-220,280)
+    mpl(-240,280,-230,290)
+    mpl(-240,279,-230,270)
+def draw_diamond():
+   
+    global d_x, d_y
+    d_x = random.randint(-240,240)
+    d_y = 260
+    glColor3f(1.0, 1.0, 1.0)
+    mpl(d_x,d_y,d_x+10,d_y+15)
+    mpl(d_x+10,d_y+15,d_x+20,d_y)
+    mpl(d_x,d_y,d_x+10,d_y-15)
+    mpl(d_x+20,d_y,d_x+10,d_y-15)
+
+
+def iterate():
+    glViewport(0, 0, 500, 600)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(-250.0, 250, -300.0, 300, 0.0, 1.0)  # Define a 2D orthographic projection
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
 
 glutInit()                               # Initialize GLUT
 glutInitDisplayMode(GLUT_RGBA)           # Set display mode: RGBA color
-glutInitWindowSize(500, 500)             # Set window size (width, height)
+glutInitWindowSize(500, 600)             # Set window size (width, height)
 glutInitWindowPosition(0, 0)             # Set window position (top-left corner)
 glutCreateWindow(b"OpenGL 2D Point")     # Create window with a title
 glutDisplayFunc(display)                 # Register display callback
